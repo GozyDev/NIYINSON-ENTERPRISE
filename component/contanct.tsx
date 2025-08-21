c0006889263c4d3b97f2a9ea23f68f51e987b4ed
GozyDev <networkinggozy@gmail.com>
2025-08-21 06:13:25 +0100

    contact page

--- component/contanct.tsx
+++ component/contanct.tsx
@@ -26,26 +26,31 @@ export default function ContactSection() {
   };
 
   return (
-    <section id="contact" className="bg--50 py-12 px-6">
+    <section id="contact" className="bg--50 py-14 pb-[100px] px-6">
       <div className="max-w-7xl mx-auto  ">
         {/* Form */}
         <div>
           <div className="flex flex-col gap-4 md:gap-0  md:flex-row  w-full  max-w-7xl">
             <div className="w-full flex flex-col  justify-center">
               <span className="space-y-3">
-                  <h2 className="text-5xl uppercase text-amber-900 font-bold  mb-[16px] tracking-tight flex items-center ">
-                    Contact me
-                  </h2>
-                  <p className="text-black/70">Reach out today custom woodwork that fits your vision starts with a conversation.</p>
-                  <a href="mailto:niyosonenterprise137@gmail.com ">
-                      <h3 className="text-xl md:text-4xl text-gray-500 animate-pulse ">niyosonenterprise137@gmail.com</h3>
-                  </a>
+                <h2 className="text-5xl uppercase text-amber-900 font-bold  mb-[16px] tracking-tight flex items-center ">
+                  Contact me
+                </h2>
+                <p className="text-black/70">
+                  Reach out today custom woodwork that fits your vision starts
+                  with a conversation.
+                </p>
+                <a href="mailto:niyosonenterprise137@gmail.com ">
+                  <h3 className="text-xl md:text-4xl text-gray-500 animate-pulse ">
+                    niyosonenterprise137@gmail.com
+                  </h3>
+                </a>
               </span>
               {/* WhatsApp & Call */}
               <div className="mt-6 flex gap-4">
                 <a
                   href="https://wa.me/2348137058129"
-                  target="_blank"  
+                  target="_blank"
                   rel="noopener noreferrer"
                   className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 flex items-center gap-2"
                 >
@@ -73,12 +78,14 @@ export default function ContactSection() {
             </div>
             <div className="w-full h-96 bg-gray-300 ">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.086458491403!2d-122.41941508468127!3d37.77492927975945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085809cbae4df1d%3A0xb3a2c4ec48d7c3a4!2sSan%20Francisco%2C%20CA!5e0!3m2!1sen!2sus!4v1692000000000!5m2!1sen!2sus"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                loading="lazy"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.951733780113!2d3.2744482747544605!3d6.6529049933418305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b90b534ef3f51%3A0xc4282020a0a6c7a0!2s120%20Old%20Ota%20Rd%2C%20Abule%20Egba%2C%20Lagos%20100276%2C%20Lagos!5e0!3m2!1sen!2sng!4v1755752709393!5m2!1sen!2sng"
+                width="600"
+                height="450"
+                style={{border:0}}
                 allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="w-full"
               ></iframe>
             </div>
           </div>
